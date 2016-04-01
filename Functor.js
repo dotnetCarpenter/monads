5d5d2ff5ae007cb84560366703cdb598c9537353
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-04-01 11:41:32 +0200

    added a List Functor based on Array

--- Functor.js
+++ Functor.js
@@ -1,8 +1,12 @@
 'use strict'
 
+// http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html
+// run with node  --harmony_rest_parameters Functor.js
+
 const plus3 = x => x+3
 const fmap = (f, F) => {
   if(F instanceof Nothing) return F
+  if(F instanceof List) return F.fmap(fmap.bind(null, f))
   return F.fmap((f(F.val)))
 }
 function Just(val) {
@@ -13,32 +17,45 @@ function Just(val) {
 function Maybe(val) {
   if(val == undefined) return new Nothing
   //if(val instanceof Just) 
-  return Just(val)
+  return new Just(val)
 }
 function Nothing() {
   if(this == undefined) return new Nothing
   this.fmap = x => new Nothing
 }
+class List extends Array {
+  constructor(...args) {
+    super(...args.map(x => Maybe(x)))
+    
+  }
+  fmap(f) {
+    return this.map(f)
+  }
+}
 
 console.log(
-  'fmap(plus3, Just(2)) ->\t',
+  'fmap(plus3, Just(2))\t-> ',
   fmap(plus3, Just(2))
 )
 console.log(
-  'fmap(plus3, Nothing()) ->\t',
+  'fmap(plus3, Nothing())\t-> ',
   fmap(plus3, Nothing())
 )
 console.log(
-  'fmap(plus3, Maybe(2)) ->\t',
+  'fmap(plus3, Maybe(2))\t-> ',
   fmap(plus3, Maybe(2))
 )
 console.log(
-  'fmap(plus3, Maybe()) -> \t',
+  'fmap(plus3, Maybe())\t-> ',
   fmap(plus3, Maybe())
 )
-console.log(findPost(), findPost(), findPost())
 console.log(
+  fmap(getPostTitle, findPost(1)),
   fmap(getPostTitle, findPost(1))
+)
+console.log(
+  new List(2,4,6)
+, fmap(plus3, new List(2,4,6))
 )
 
 function getPostTitle (post) { return post.title }
