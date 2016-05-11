12ba6e6d793b53abdd3ff24dda6b7736b94a44f8
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-05-11 21:47:08 +0200

    like might work better than same

--- Functor.js
+++ Functor.js
@@ -11,7 +11,7 @@ const plus3 = x => x+3
 // Functors
 const fmap = (f, F) => {
   if(F instanceof Nothing) return F
-  if(F instanceof List) return F.fmap(fmap.bind(null, f))
+  // if(F instanceof List) return F.fmap(fmap.bind(null, f))
   return F.fmap((f(F.val)))
 }
 function Just(val) {
@@ -27,17 +27,17 @@ function Nothing() {
   if(!new.target) return new Nothing
   this.fmap = x => new Nothing
 }
-class List extends Array {
-  constructor(...args) {
-    super(...args.map(x => Maybe(x)))    
-  }
-  fmap(f) {
-    return this.map(f)
-  }
-}
+// class List extends Array {
+//   constructor(...args) {
+//     super(...args.map(x => Maybe(x)))    
+//   }
+//   fmap(f) {
+//     return this.map(f)
+//   }
+// }
 
 // Tests
-tap.same(fmap(plus3, Just(2)), Just(5), "fmap knows how to apply functions to values that are wrapped in a context.")
+tap.like(fmap(plus3, Just(2)), Just(5), "fmap knows how to apply functions to values that are wrapped in a context.")
 console.log(
   'fmap(plus3, Just(2))\t-> ',
   fmap(plus3, Just(2))
@@ -46,32 +46,32 @@ console.log(
   'fmap(plus3, Nothing())\t-> ',
   fmap(plus3, Nothing())
 )
-console.log(
-  'fmap(plus3, Maybe(2))\t-> ',
-  fmap(plus3, Maybe(2))
-)
-console.log(
-  'fmap(plus3, Maybe())\t-> ',
-  fmap(plus3, Maybe())
-)
+// console.log(
+//   'fmap(plus3, Maybe(2))\t-> ',
+//   fmap(plus3, Maybe(2))
+// )
+// console.log(
+//   'fmap(plus3, Maybe())\t-> ',
+//   fmap(plus3, Maybe())
+// )
 
-console.log('Run findPost two times. findPost sometimes return null but the return value will always be wrapped in a Maybe')
-console.log(
-  fmap(getPostTitle, findPost(1)),
-  fmap(getPostTitle, findPost(1))
-)
+// console.log('Run findPost two times. findPost sometimes return null but the return value will always be wrapped in a Maybe')
+// console.log(
+//   fmap(getPostTitle, findPost(1)),
+//   fmap(getPostTitle, findPost(1))
+// )
 
-console.log('What happens when you apply a function to a list')
-console.log(
-  new List(2,4,6)
-)
-console.log(
-  fmap(plus3, new List(2,4,6))
-)
+// console.log('What happens when you apply a function to a list')
+// console.log(
+//   new List(2,4,6)
+// )
+// console.log(
+//   fmap(plus3, new List(2,4,6))
+// )
 
-function getPostTitle (post) { return post.title }
-function findPost(n) {
-  return new Maybe(
-    Math.round(Math.random()) ? { title: 'Anakia' } : null
-  )
-}
+// function getPostTitle (post) { return post.title }
+// function findPost(n) {
+//   return new Maybe(
+//     Math.round(Math.random()) ? { title: 'Anakia' } : null
+//   )
+// }
