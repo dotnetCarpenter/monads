5f5612dc154b21d1d9c31d0905048fb4b7147cb3
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-05-20 00:59:53 +0200

    finally get Functors like 80%

--- Functor.js
+++ Functor.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict"
 
 // http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html
 // run with node  --harmony_rest_parameters Functor.js
@@ -6,65 +6,66 @@
 const tap = require("tap")
 
 // Test function
-const plus3 = x => x+3
-const plus2 = x => x+2
+const plus3 = x => x + 3
+const plus2 = x => x + 2
+const minus15 = x => x - 15
 
 // Functors
 // (a->b), fa -> fb
-const fmap = (f, F) => {
-	// if(F instanceof Nothing) return F
-  // if(F instanceof List) return F.fmap(fmap.bind(null, f))
-  // return F.fmap((f(F.val)))
-	return F.fmap( x => f(x) )
-}
+const fmap = (f, F) => F.fmap( x => f(x) )
+
 function Just(val) {
-  return {
-		fmap: f => f(val) 
-	}
+	if(!new.target) return new Just(val)
+  this.fmap = f => f(val)
 }
-fmap(plus3, Just(2))
+
 function Maybe(val) {
-  if(val == undefined) return new Nothing
-  return new Just(val)
+  return val == undefined ? new Nothing : new Just(val)
 }
+
 function Nothing() {
   if(!new.target) return new Nothing
-  this.fmap = x => new Nothing
+  this.fmap = () => Nothing
 }
+
 class List extends Array {
   constructor(...args) {
-    super(...args.map(x => Maybe(x)))    
+    super(...args.map(x => Maybe(x)))
   }
   fmap(f) {
-    return this.map(f)
+    return this.map(fa => fa.fmap(f))
   }
 }
-// Function.prototype.fmap = f => x => f(x)
 
+Function.prototype.fmap = function(f) {
+	const self = this
+	return x => self(f(x))
+}
 
 // Tests
-tap.like( fmap(plus3, Just(2)), {val:5,fmap:Function}, "fmap knows how to apply functions to values that are wrapped in a context.")
-// tap.like( fmap(plus3, Nothing()), {val:null,fmap:Function}, "you start with Nothing, and you end up with Nothing!")
-// tap.like( fmap(plus3, Maybe(2)), {val:5,fmap:Function}, "Maybe is a Functor. It specifies how fmap applies to Justs and Nothings.")
-// tap.like( fmap(plus3, Maybe()), {val:undefined,fmap:Function}, "Maybe is a Functor. It specifies how fmap applies to Justs and Nothings.")
+tap.like( Just(2), Just, "fmap knows how to apply functions to values that are wrapped in a context.")
+tap.like( fmap(plus3, Just(2)), 5, "fmap knows how to apply functions to values that are wrapped in a context.")
 
+tap.like( fmap(plus3, Nothing()), Nothing, "you start with Nothing, and you end up with Nothing!")
 
-// console.log('Run findPost two times. findPost sometimes return null but the return value will always be wrapped in a Maybe')
-// console.log(
-//   fmap(getPostTitle, findPost(1)),
-//   fmap(getPostTitle, findPost(1))
-// )
+tap.like( Maybe(2), Just, "Maybe is a Functor. It specifies how fmap applies to Justs and Nothings.")
+tap.like( Maybe(), Nothing, "Maybe is a Functor. It specifies how fmap applies to Justs and Nothings.")
+tap.like( fmap(plus3, Maybe(2)), 5, "Maybe is a Functor. It specifies how fmap applies to Justs and Nothings.")
+tap.like( fmap(plus3, Maybe()), Nothing, "Maybe is a Functor. It specifies how fmap applies to Justs and Nothings.")
 
-// console.log('What happens when you apply a function to a list')
-// tap.like( new List(2,4,6) , [{val:2,fmap:Function},{val:4,fmap:Function},{val:6,fmap:Function}] , "should be Just objects." )
-// tap.like( fmap(plus3, new List(2,4,6)) , [{val:5,fmap:Function},{val:7,fmap:Function},{val:9,fmap:Function}] , "should be Just objects with a value plus 3 higher than input." )
+tap.like( fmap(getPostTitle, findPost(1)), 'Anakia', "findPost sometimes return null but the return value will always be wrapped in a Maybe")
+tap.like( fmap(getPostTitle, findPost(2)), Nothing, "findPost sometimes return null but the return value will always be wrapped in a Maybe")
 
-// const foo = fmap( plus3, plus2 )
-// tap.equal(foo(10), 15, "So functions are Functors too!")
+tap.like( new List(2,4,6) , [Just,Just,Just] , "What happens when you apply a function to a list? Should be Just objects." )
+tap.like( fmap(plus3, new List(2,4,6)) , [5,7,9] , "What happens when you apply a function to a list? 3 higher than input." )
+tap.like( fmap(plus3, new List(2,null,6)) , [5,Nothing,9] , "What happens when you apply a function to a list? Null values are wrapped in Nothing" )
+
+const foo = fmap( plus3, plus2 )
+tap.like(foo(10), 15, "So functions are Functors too!")
 
 function getPostTitle (post) { return post.title }
 function findPost(n) {
   return new Maybe(
-    Math.round(Math.random()) ? { title: 'Anakia' } : null
+    n === 1 ? { title: 'Anakia' } : null
   )
 }
