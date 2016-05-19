c607bf7a487d59f10407184c5eb03645e5396c8e
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-05-19 21:39:25 +0200

    tried to be clever with prototypal object-oriented programming

--- Functor2.js
+++ Functor2.js
@@ -3,33 +3,47 @@
 // http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html
 // run with node  --harmony_rest_parameters Functor.js
 
-const tap = require("tap")
+// const tap = require("tap")
 
 // Test function
 const plus3 = x => x+3
 
 // Functors
+
+// const context = Object.create({ val: undefined, fmap })
 const fmap = (f, F) => {
-  if(F instanceof Nothing) return F
+  // if(F.isPrototypeOf(Nothing)) return F
   // if(F instanceof List) return F.fmap(fmap.bind(null, f))
-  return F.fmap((f(F.val)))
+  // return F.fmap((f(F.val)))
+  return F.fmap(f, F)
 }
-function Just(val) {
-  if(!new.target) return new Just(val)
-  this.val = val
-  this.fmap = x => new Just(x)
+const Just = {
+  val: undefined,
+  fmap: (a, fa) => {
+    this.val = a(fa.val) 
+    return this
+  }
 }
-function Maybe(val) {
-  if(val == undefined) return new Nothing
-  return new Just(val)
-}
-function Nothing() {
-  if(!new.target) return new Nothing
-  this.fmap = x => new Nothing
-}
+// function just(val) {
+//   return Object.create({fmap: (a, fa) => Just(a(fa.val))}, {
+//     "val": { value: val }
+//   })
+// }
+// function Nothing() {
+//   return Object.create(Nothing, {
+//     "val": { value: null },
+//     fmap: (a, fa) => Nothing()
+//   })
+// }
+// function Maybe(val) {
+//   return val => val == null ? Nothing() : Just(val)
+// }
+const just = Object.create(Just)
+just.val = 2
+fmap(plus3, just)
 // class List extends Array {
 //   constructor(...args) {
-//     super(...args.map(x => Maybe(x)))    
+//     super(...args.map(x => Maybe(x)))
 //   }
 //   fmap(f) {
 //     return this.map(f)
@@ -37,7 +51,7 @@ function Nothing() {
 // }
 
 // Tests
-tap.like(fmap(plus3, Just(2)), Just(5), "fmap knows how to apply functions to values that are wrapped in a context.")
+// tap.like(fmap(plus3, Just(2)), Just(5), "fmap knows how to apply functions to values that are wrapped in a context.")
 console.log(
   'fmap(plus3, Just(2))\t-> ',
   fmap(plus3, Just(2))
@@ -50,10 +64,10 @@ console.log(
 //   'fmap(plus3, Maybe(2))\t-> ',
 //   fmap(plus3, Maybe(2))
 // )
-// console.log(
-//   'fmap(plus3, Maybe())\t-> ',
-//   fmap(plus3, Maybe())
-// )
+console.log(
+  'fmap(plus3, Maybe())\t-> ',
+  fmap(plus3, Maybe())
+)
 
 // console.log('Run findPost two times. findPost sometimes return null but the return value will always be wrapped in a Maybe')
 // console.log(
