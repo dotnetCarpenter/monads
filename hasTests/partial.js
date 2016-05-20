599f0223e94b7dc5028e5a2429b4381f353b9a07
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-05-20 22:10:53 +0200

    made the function partial to make working with curry nicer

--- hasTests/partial.js
+++ hasTests/partial.js
@@ -6,9 +6,12 @@ const tap = require("tap")
 const curry = require("./curry")
 const add = (a,b, c) => a + b + c
 
-function partial(f) {}
+function partial(f, ...args) {
+	return curry(f).bind(f, ...args)
+}
 
 tap.equal( curry(add)(1)(2)(3), 6, "should be equal to 6")
+
 let a = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 const take = (n, list, accu = []) => {
 	if(n === 0) return accu
@@ -16,7 +19,5 @@ const take = (n, list, accu = []) => {
 	return take( n - 1, list.slice(1), accu )
 }
 
-const take5 = curry(take)(5)
-
-tap.same(take(5, a), take5(a))
-
+const take5 = partial(take, 5)
+tap.same(take5(a), take(5, a), "A dyadic function should return the same as a partial monadic function made of that dyadic function.")
