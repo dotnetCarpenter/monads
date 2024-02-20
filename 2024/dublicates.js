a3caec1a5822f3fb1a8c10ce72e492fc292bc890
dotnetCarpenter <jon.ronnenberg@gmail.com>
2024-02-20 23:57:53 +0100

    fix type signatures and spaces (because vscode is always lying)

--- 2024/dublicates.js
+++ 2024/dublicates.js
@@ -1,9 +1,6 @@
 "use strict"
 
-// postfix ? signifies a nullable value
-// e.g. a? is either a or null
-
-// 	  head :: [a] -> a
+//    head :: [a] -> a
 const head = as => as[0]
 
 //    tail :: [a] -> [a]
@@ -15,43 +12,40 @@ const push = cs => c => (cs.push (c), cs)
 //    isIn :: [a] -> a -> Boolean
 const isIn = cs => c => Boolean (cs.indexOf (c) > -1)
 
-//    iterator :: (a -> a?) -> a -> a
+//    iterator :: (a -> b) -> [a] -> a
 const iterator = f => str => f (head (str)) === null
-								? iterate (f)
-									      (tail (str))
-								: head (str)
+				? iterate (f)
+				          (tail (str))
+				: head (str)
 
-//    iterate :: (a -> a?) -> a -> a
+//    iterate :: (a -> b) -> [a] -> a
 const iterate = f => str => str === ""
-							? "Not found"
-							: iterator	(f)
-										(str)
+			    ? "Not found"
+			    : iterator (f)
+				       (str)
 
-//    dupl2 :: a -> a
-const dupl2 = str => {
+//    dupl1 :: [a] -> b
+const dupl1 = str => {
 	const seen = []
 	const isSeen = isIn (seen)
 	const addToSeen = push (seen)
 
 	return iterate (c => isSeen (c)
-							? c
-							: (addToSeen (c), null))
-				   (str)
+			     ? c
+			     : (addToSeen (c), null))
+		       (str)
 }
-
-function dupl1 (str) {
+//       dupl2 :: [a] -> b
+function dupl2 (str) {
 	const seen = []
-	const isSeen = isIn (seen)
-	const addToSeen = push (seen)
 	let c = ""
 
 	for (var i = 0, max = str.length; i < max; ++i) {
-		c = head (str)
-		str = tail (str)
-		if (isSeen (c)) {
+		c = str[i]
+		if (seen.indexOf (c) > -1) {
 			return c
 		} else {
-			addToSeen (c)
+			seen.push (c)
 		}
 	}
 
@@ -63,16 +57,16 @@ const test2 = "abcdefcf"
 const test3 = "abc"
 let ts = new Array (3)
 
-console.time ("dupl1")
-ts[0] = dupl1 (test1)
-ts[1] = dupl1 (test2)
-ts[2] = dupl1 (test3)
-console.timeEnd ("dupl1")
-console.debug (...ts)
-
 console.time ("dupl2")
 ts[0] = dupl2 (test1)
 ts[1] = dupl2 (test2)
 ts[2] = dupl2 (test3)
 console.timeEnd ("dupl2")
+console.debug (...ts)
+
+console.time ("dupl1")
+ts[0] = dupl1 (test1)
+ts[1] = dupl1 (test2)
+ts[2] = dupl1 (test3)
+console.timeEnd ("dupl1")
 console.debug (...ts)
